ca617d55b576df612e681e6985f5eab900ba19db
amirosimani <amirhs.imani@gmail.com>
2016-02-22 15:07:08 -0500

    clean data
    data ready for analysis

--- lab1.R
+++ lab1.R
@@ -1,9 +1,19 @@
 ### Libraries----
 library(data.table)
+library(dplyr)
 
 ### import data----
-network <- read.csv("http://moreno.ss.uci.edu/krackht.dat", header=TRUE, sep=",", skip= 7)
-attributes <- read.csv("Kracht_attributes.csv", header=TRUE, sep=";", skip= 8)
+network <- read.csv("http://moreno.ss.uci.edu/krackht.dat", header=T, sep=";", skip= 7)
+network = setDT(network)[, tstrsplit(DATA., ' ')]
+network = network[,V1:=NULL]
 
-clmns <- strsplit(as.numeric(network[1]),'') 
-setDT(network[, paste0("clmns", 1:16) := tstrsplit(clmns, ",")])
+#splitting the network data to advice/friendship/report
+advice <- slice(network, 1:21)
+friendship <- slice(network, 22:42)
+report <- slice(network, 43:63)
+
+#importing attributes data
+attributes <- read.csv("Kracht_attributes.csv", header=TRUE, sep=";")
+
+
+### 
