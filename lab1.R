8c2b2880621ba9609d552cbab147f71d57e3999a
amirosimani <amirhs.imani@gmail.com>
2016-02-21 23:58:55 -0500

    attributes

--- lab1.R
+++ lab1.R
@@ -3,7 +3,7 @@ library(data.table)
 
 ### import data----
 network <- read.csv("http://moreno.ss.uci.edu/krackht.dat", header=TRUE, sep=",", skip= 7)
-attributes <- read.csv("http://moreno.ss.uci.edu/krackht_att.dat", header=TRUE, sep=",", skip= 8)
+attributes <- read.csv("Kracht_attributes.csv", header=TRUE, sep=";", skip= 8)
 
 clmns <- strsplit(as.numeric(network[1]),'') 
 setDT(network[, paste0("clmns", 1:16) := tstrsplit(clmns, ",")])
